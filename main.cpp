027ea8dcf9daee4f549f163b0d651d8615e6c133
Stanislau Hlebik <stash@fb.com>
2016-02-26 18:46:27 +0000

    Not working libevent accept

--- main.cpp
+++ main.cpp
@@ -10,11 +10,39 @@
 #include <stdio.h>
 #include <errno.h>
 
+#include <unistd.h>
+
+#include <string>
+
 #define PORT 1234
+
+void shutdown_connection(evutil_socket_t sock) {
+    shutdown(sock, SHUT_RDWR);
+    close(sock);
+}
 
 void listener_cb(evconnlistener* listener,
     evutil_socket_t sock, sockaddr* addr, int len, void* ptr) {
     std::cout << "Accepted" << std::endl;
+    char* buf = new char[10000];
+    auto res = read(sock, buf, 10000);
+    if (-1 == res) {
+        perror("Read failed");
+        shutdown_connection(sock);
+        return;
+    } else {
+        std::cout << std::string(buf, res) << std::endl;
+    }
+
+    char outbuf[] =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html; charset=UTF-8\r\n"
+        "Content-Length: 4\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "aaaa";
+    write(sock, outbuf, sizeof(outbuf));
+    shutdown_connection(sock);
 }
 
 int main() {
@@ -23,12 +51,11 @@ int main() {
     sa.sin_family = AF_INET;
     sa.sin_port = htons(PORT);
     sa.sin_addr.s_addr = htonl(INADDR_ANY);
-    // inet_pton(AF_INET, "127.0.0.1", &sa);
 
     auto listener = evconnlistener_new_bind(base,
         listener_cb /*callback*/,
         NULL /*arg*/,
-        LEV_OPT_CLOSE_ON_FREE, // Maybe make blocking?
+        LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE | LEV_OPT_LEAVE_SOCKETS_BLOCKING, // TODO(stash): Maybe make non-blocking?
         -1,
         (sockaddr*)&sa,
         sizeof(sockaddr_in)
