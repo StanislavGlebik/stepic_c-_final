c693b0da24326ff172d0acdda54b42158e07442a
Stanislau Hlebik <stash@fb.com>
2016-02-26 20:45:57 +0100

    Add worker thread

--- main.cpp
+++ main.cpp
@@ -16,8 +16,10 @@
 #include <unistd.h>
 
 #include <string>
+#include <thread>
 
 #define PORT 1234
+#define POOL_SIZE 1
 
 void shutdown_connection(evutil_socket_t sock) {
     shutdown(sock, SHUT_RDWR);
@@ -46,12 +48,41 @@ void listener_cb(evutil_socket_t sock) {
         "aaaa";
     write(sock, outbuf, sizeof(outbuf));
     shutdown_connection(sock);
+    std::cout << "Handled by: " << std::this_thread::get_id() << std::endl;
 }
 
+class WorkerThread {
+public:
+    WorkerThread(ThreadQueue<int>& q) : q_(q) {
+    }
+    
+    WorkerThread(const WorkerThread& w) : q_(w.q_) {
+    }
+
+    void operator()() {
+        while (true) {
+            q_.Pop();
+            std::cout << "Got" << std::endl;
+        }
+    }
+
+private:
+    ThreadQueue<int>& q_;
+};
+
 int main() {
+    std::cout << "Main thread: " << std::this_thread::get_id() << std::endl;
     ThreadQueue<int> t_q;
-    t_q.Push(0);
-    std::cout << t_q.Pop() << std::endl;
+    // TODO(stash): why cannot pass WorkerThread here (copy constructor error)?
+    std::thread worker([&t_q] ()
+        {
+            while (true) {
+                auto x = t_q.Pop();
+                listener_cb(x);
+            }
+        }
+    );
+
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("socket failed");
@@ -77,13 +108,14 @@ int main() {
     }
     
     while (true) {
+        std::cout << "Before accept" << std::endl;
         auto accepted_sock = accept(sock, NULL, NULL);
         if (-1 == accepted_sock) {
             perror("accept failed");
             close(accepted_sock);
             continue;
         }
-
-        listener_cb(accepted_sock);        
+        t_q.Push(accepted_sock);
     }
+    worker.join();
 }
